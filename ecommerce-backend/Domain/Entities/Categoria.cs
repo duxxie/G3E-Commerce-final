7f43020302e0103d05a51158cbe0b2ee376e3589
Gabriel Felipe <gabri26.cost@gmail.com>
2026-01-30 12:47:09 -0300

    Classes revisadas. Classes sendo configuradas para o banco de dados.

--- ecommerce-backend/Domain/Entities/Categoria.cs
+++ ecommerce-backend/Domain/Entities/Categoria.cs
@@ -3,6 +3,6 @@ namespace Domain.Entities
     class Categoria
     {
         public int id { get; private set; }
-        public string nome { get; private set; }
+        public string? nome { get; private set; }
     }
 }
--- ecommerce-backend/Infrastructure/Data/EcommerceDbContext.cs
+++ ecommerce-backend/Infrastructure/Data/EcommerceDbContext.cs
@@ -1,3 +1,4 @@
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -7,6 +8,12 @@ namespace Infrastructure.Data
         public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
         {
         }
-        // Entities
+        // Entidades
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<ItemCarrinho> ItensCarrinho { get; set; }
+        public DbSet<ItemPedido> ItensPedido { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
